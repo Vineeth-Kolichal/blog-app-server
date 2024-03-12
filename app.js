155e784be8d1e6c4ac10ec3d772f5321e7756e3e
Vineeth Chandran <vineeth.chandran@toobler.com>
2024-03-12 17:53:55 +0530

    swagger json created

--- app.js
+++ app.js
@@ -5,9 +5,14 @@ const swaggerDoc=YAML.load("./api_doc/swagger.yaml")
 const postRoute=require("./routes/postRoutes");
 const userRoute = require("./routes/userRoute");
 const imageRoute = require("./routes/imageRoute");
-
+const sjson=require("./api_doc/swagger.json")
 const app=express();
 
+
+console.log(sjson);
+console.log(swaggerDoc)
+console.log(typeof(swaggerDoc))
+console.log(typeof(sjson))
 app.use(express.json())
 app.use("/api/uploads",express.static('uploads'))
 app.use("/api/documentation",swaggerUi.serve,swaggerUi.setup(swaggerDoc))
