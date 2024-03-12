e1e4e0b4d68007781056a6275393e763dbfd9bf6
Vineeth Chandran <vineeth.chandran@toobler.com>
2024-03-12 15:39:28 +0530

    some changes

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -6,7 +6,6 @@ const verifyToken=(req,res,next)=>{
 
     try {
         const token=req.headers.authorization.split(" ")[1];
-       
         const userData=jwt.verify(token,process.env.JWT_SECRET);
         req.userData=userData;
         next();
